2f79685ee2c2f92bb1cc5ebfdafdaf057617165e
Xrysnow <xrysnow@outlook.com>
2021-09-22 16:31:21 +0800

    handle ModCallbacks

--- src/enums.lua
+++ src/enums.lua
@@ -10,6 +10,92 @@ local ROOMSHAPE = {
     ROOMSHAPE_LBR = '┏',
 }
 
+local function procArgs(s)
+    s = s:trim()
+    s = s:gsub('^%(', '')
+    s = s:gsub('%)%)$', ')')
+    s = s:gsub('%b()', '')
+    s = s:gsub(' %[int%]', '(int)')
+    s = s:gsub(' %[string%]', '(string)')
+    s = s:gsub(' %[bool%]', '(bool)')
+    s = s:gsub(' %[Vector%]', '(Vector)')
+    s = s:gsub(' %[float%]', '(float)')
+    s = s:gsub(' %[%[PillEffect%]%]', '(PillEffect)')
+    s = s:gsub('%)%)$', ')')
+    s = s:gsub('<br>', '')
+    s = s:gsub('%[', '')
+    s = s:gsub('%]', '')
+    s = s:gsub(' Entity,', '(Entity),')
+    s = s:gsub('PillColor%)$', 'PillColor')
+    s = s:gsub('Curses%)$', 'Curses')
+    return s
+end
+
+local function procModCallbacks(content)
+    local results = {}
+    local function append(l)
+        table.insert(results, l)
+    end
+    local enum_name = 'ModCallbacks'
+    append(('---@class %s @enum'):format(enum_name))
+    append(('%s = {}'):format(enum_name))
+    append('')
+    ---@type string[]
+    local lines = string.split(content, '\n')
+    local cur_desc = {}
+    for iline, line in ipairs(lines) do
+        local is_enum = line:starts_with('### ')
+        local is_proto = line:starts_with('|[ ](#)')
+        if is_enum then
+            cur_desc = {}
+        elseif is_proto then
+            -- flush desc
+            for _, s in ipairs(cur_desc) do
+                append(('--- %s'):format(utils.procDescLine(s)))
+            end
+            --
+            local words = line:split('|')
+            for i = 1, #words do
+                words[i] = words[i]:trim()
+            end
+            --local tags = words[2]:sub(10)
+            local value = words[3]:gsub(' ', ''):gsub('^1<<', '2^')
+            local name = words[4]:gsub(' {.*$', ''):trim()
+            local FunctionArgs = words[5] or ''
+            local OptionalArgs = words[6] or ''
+            --
+            if FunctionArgs == '-' then
+                FunctionArgs = ''
+            end
+            if OptionalArgs == '-' then
+                OptionalArgs = ''
+            end
+            if FunctionArgs ~= '' then
+                FunctionArgs = procArgs(FunctionArgs)
+                --append('---')
+                append(('--- Function args: %s'):format(FunctionArgs))
+            end
+            if OptionalArgs ~= '' then
+                OptionalArgs = procArgs(OptionalArgs)
+                append('---')
+                append(('--- Optional args: %s'):format(OptionalArgs))
+            end
+            --
+            append('---')
+            append(('--- %s'):format(words[3]:gsub(' ', '')))
+            append(('%s.%s = %s'):format(enum_name, name, value))
+        else
+            if line:sub(1, 1) ~= '|' then
+                table.insert(cur_desc, line)
+            end
+        end
+    end
+    append('')
+    append(('return %s'):format(enum_name))
+    append('')
+    return table.concat(results, '\n')
+end
+
 local function proc(content)
     local results = {}
     local function append(l)
@@ -22,6 +108,10 @@ local function proc(content)
         local enum_decl = line:match('^# Enum "(.*)"$')
         local is_proto = line:starts_with('|[ ](#)')
         if enum_decl then
+            if enum_decl == 'ModCallbacks' then
+                -- special
+                return procModCallbacks(content)
+            end
             enum_name = enum_decl
             if enum_name == 'ItemConfig' then
                 -- workaround
